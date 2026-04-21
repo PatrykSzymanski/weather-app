23ef9b4683be1b69539be7ffa3a4a01727102c15
Patryk Szymański <patryk_szymanski@icloud.com>
2026-04-21 15:57:27 +0200

    Update MainlyClear icon positioning and scaling for improved visual representation

--- app/components/icons/weather/MainlyClear.tsx
+++ app/components/icons/weather/MainlyClear.tsx
@@ -6,7 +6,8 @@ export function MainlyClear({ isNight, className }: WeatherIconProps) {
   return (
     <WeatherIcon className={className}>
       {isNight ? <Moon x={20} y={-10} /> : <Sun x={20} y={-10} />}
-      <Cloud x={-20} y={30} scale={0.7} />
+      <Cloud x={75} y={5} scale={0.5} />
+      <Cloud x={-65} y={65} scale={0.6} />
     </WeatherIcon>
   );
 }
